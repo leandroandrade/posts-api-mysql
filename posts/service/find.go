053176d74621be4c695a9dc76742f866fede98d8
leandroandrade <leandro.andrade2588@gmail.com>
2018-06-18 19:05:29 -0300

    improvements

--- posts/service/find.go
+++ posts/service/find.go
@@ -6,7 +6,7 @@ import (
 	"github.com/leandroandrade/posts-api-mysql/posts/model"
 )
 
-func FindAll() ([]model.Post, error) {
+func FindAll() ([]*model.Post, error) {
 	posts, err := getAllPosts()
 	if err != nil {
 		return nil, err
@@ -15,21 +15,21 @@ func FindAll() ([]model.Post, error) {
 	return posts, nil
 }
 
-func getAllPosts() ([]model.Post, error) {
+func getAllPosts() ([]*model.Post, error) {
 	rows, err := mysql.DB.Query("select * from post")
 	if err != nil {
 		return nil, errors.New(err.Error())
 	}
 	defer rows.Close()
 
-	var posts []model.Post
-	var post model.Post
+	posts := make([] *model.Post, 0)
 
 	for rows.Next() {
+		var post model.Post
 		if err = rows.Scan(&post.Id, &post.Description, &post.DatePosted); err != nil {
 			return nil, err
 		}
-		posts = append(posts, post)
+		posts = append(posts, &post)
 	}
 
 	return posts, err
