58f56a1a15bcea9199e15829136d540cf759a700
leandroandrade <leandro.andrade2588@gmail.com>
2018-06-28 20:42:37 -0300

    new test case

---
+++ response/message_test.go
@@ -0,0 +1,54 @@
+package response
+
+import (
+	"testing"
+	"net/http/httptest"
+)
+
+type responseExample struct {
+	Message string
+}
+
+func TestJSONErr(t *testing.T) {
+	var testcases = []struct {
+		writer  *httptest.ResponseRecorder
+		payload Payload
+	}{
+		{
+			writer:  httptest.NewRecorder(),
+			payload: Payload{Code: 200, Message: "Success", Detail: "Operation sucessful"},
+		},
+		{
+			writer:  httptest.NewRecorder(),
+			payload: Payload{Code: 400, Message: "Request error", Detail: "Cannot complete your request"},
+		},
+	}
+
+	for _, test := range testcases {
+		JSONErr(test.writer, test.payload)
+		if test.payload.Code != test.writer.Code {
+			t.Errorf("FAIL: JSONErr code %v, want %v", test.writer.Code, test.payload.Code)
+		}
+	}
+}
+
+func TestJSON(t *testing.T) {
+	var testcases = []struct {
+		writer *httptest.ResponseRecorder
+		body   responseExample
+		code   int
+	}{
+		{
+			writer: httptest.NewRecorder(),
+			body:   responseExample{Message: "Hello"},
+			code:   200,
+		},
+	}
+
+	for _, test := range testcases {
+		JSON(test.writer, test.code, test.body)
+		if test.code != test.writer.Code {
+			t.Errorf("FAIL: JSONErr code %v, want %v", test.writer.Code, test.code)
+		}
+	}
+}
